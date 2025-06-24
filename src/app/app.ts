7676305b82c39c0d6b820ee366c911455da1eb41
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-06-24 17:36:47 +0530

    How To make a Costom Componete and How to User Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { ProfileComponent } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, Signup, ProfileComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-part1';
-  name = 'amanpatel';
-  pd = this.hrllo1();
-
-  user1 = 'aman' .toUpperCase();
-  user2 = 'patel';
-  user3 = 'ambalal';
-
-  hrllo1() {
-    const a = 10;
-    console.log('hello1');
-  }
 }
